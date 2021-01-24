435af57677d06a617453fb64d7f1dce4eb960abe
Alexey Bukhtiyarov <a.bukhtiyarov@yandex.ru>
2021-01-24 14:07:26 +0300

    clustering eval

---
+++ src/evaluate_clustring.py
@@ -0,0 +1,118 @@
+import argparse
+import json
+import tqdm
+import numpy as np
+
+from _jsonnet import evaluate_file as jsonnet_evaluate_file
+from transformers import AutoTokenizer, EncoderDecoderModel, logging
+from sklearn.cluster import AgglomerativeClustering
+
+
+from evaluationclustering_utils import get_gold_markup, get_data_to_cluster, doc2vec_bert, calc_clustering_metrics
+from models.bottleneck_encoder_decoder import BottleneckEncoderDecoderModel
+
+
+def get_text_to_vector_func(text_to_vec_func, model, tokenizer):
+    if text_to_vec_func == 'bert-MeanSum':
+        return lambda doc: doc2vec_bert(doc, model, tokenizer, 'MeanSum')
+    elif text_to_vec_func == 'bert-FirstCLS':
+        return lambda doc: doc2vec_bert(doc, model, tokenizer, 'FirstCLS')
+    else:
+        raise NotImplementedError
+
+
+def get_clf_report(embeds, markup, url2record, dist_threshold):
+    clustering_model = AgglomerativeClustering(
+        n_clusters=None,
+        distance_threshold=dist_threshold,
+        linkage="single",
+        affinity="cosine"
+    )
+
+    clustering_model.fit(embeds)
+    labels = clustering_model.labels_
+
+    id2url = dict()
+    for i, (url, _) in enumerate(url2record.items()):
+        id2url[i] = url
+
+    url2label = dict()
+    for i, label in enumerate(labels):
+        url2label[id2url[i]] = label
+
+    return calc_clustering_metrics(markup, url2label, url2record, output_dict=True), \
+        calc_clustering_metrics(markup, url2label, url2record, output_dict=False)
+
+
+def get_quality(embeds, markup, url2record, dist_threshold, print_result=False):
+    dict_report, str_report = get_clf_report(embeds, markup, url2record, dist_threshold)
+
+    if print_result:
+        print(str_report)
+
+    return dict_report['accuracy']
+
+
+def perform_clustering_eval(config_file,
+                            eval_model_file,
+                            clustering_data_file,
+                            gold_markup_file,
+                            enable_bottleneck,
+                            text_to_vec_func
+):
+    logging.set_verbosity_info()
+
+    config = json.loads(jsonnet_evaluate_file(config_file))
+
+    tokenizer_model_path = config.pop("tokenizer_model_path")
+    tokenizer = AutoTokenizer.from_pretrained(tokenizer_model_path, do_lower_case=False)
+
+    max_tokens_text = config.pop("max_tokens_text", 196)
+
+    print("Loading model...")
+    cls = BottleneckEncoderDecoderModel if enable_bottleneck else EncoderDecoderModel
+    model = cls.from_pretrained(eval_model_file)
+    model.eval()
+
+    gold_markup = get_gold_markup(gold_markup_file)
+    url2record, filename2url = get_data_to_cluster(clustering_data_file)
+    setattr(tokenizer, 'max_tokens_text', max_tokens_text)
+    text_to_vector_func = get_text_to_vector_func(text_to_vec_func, model, tokenizer)
+
+    print('Calculating embeddings...')
+    embeds = []
+
+    for i, (url, record) in tqdm.tqdm(enumerate(url2record.items())):
+        text = record["title"] + ' ' + record["text"]
+        text = text.lower().replace('\xa0', ' ')
+        embeds.append(text_to_vector_func(text))
+
+    embeds = np.array(embeds)
+    assert len(embeds) == len(url2record.items())
+
+    print('Searching for optimal threshold')
+    domain = np.logspace(-3, 0, 11)
+    quals = [get_quality(embeds, gold_markup, url2record, dist)
+            for dist in tqdm.tqdm(domain, total=11)]
+
+    closer_domain = np.linspace(domain[max(0, np.argmax(quals) - 2)], domain[min(np.argmax(quals) + 3, len(domain) - 1)], 9)
+    closer_quals = [get_quality(embeds, gold_markup, url2record, dist)
+                    for dist in tqdm.tqdm(closer_domain, total=9)]
+
+    best_dist = closer_domain[np.argmax(closer_quals)]
+
+    print('\nBest distance =', best_dist)
+    get_quality(embeds, gold_markup, url2record, best_dist, print_result=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config-file", type=str, required=True)
+    parser.add_argument("--eval-model-file", type=str, required=True)
+    parser.add_argument("--clustering-data-file", type=str, required=True)  # ru_clustering_data.jsonl
+    parser.add_argument("--gold-markup-file", type=str, required=True)  # ru_threads_target.tsv
+    parser.add_argument("--enable-bottleneck", default=False, action='store_true')
+    parser.add_argument("--text-to-vec-func", type=str, default='bert', choices=('bert', 'distill'))
+
+    args = parser.parse_args()
+    perform_clustering_eval(**vars(args))
---
+++ src/evaluation/clustering_utils.py
@@ -0,0 +1,70 @@
+from razdel import sentenize
+import torch
+import csv
+import json
+from collections import defaultdict
+from sklearn.metrics import classification_report
+
+
+def doc2vec_bert(text, model, tokenizer, mode='MeanSum'):
+    inp = torch.LongTensor(tokenizer.encode(text))[:tokenizer.max_token_text].unsqueeze(0)
+    output = model.encoder(inp)['last_hidden_state'][0]
+
+    if mode == 'FirstCLS':
+        return output[0]
+    elif mode == 'MeanSum':
+        return output.mean(0)
+    else:
+        raise Exception('Wrong mode')
+
+
+def read_markup(file_name):
+    with open(file_name, "r") as r:
+        reader = csv.reader(r, delimiter='\t', quotechar='"')
+        header = next(reader)
+        for row in reader:
+            assert len(header) == len(row)
+            record = dict(zip(header, row))
+            yield record
+
+
+def get_gold_markup(markup_path):
+    markup = defaultdict(dict)
+    for record in read_markup(markup_path):
+        first_url = record["INPUT:first_url"]
+        second_url = record["INPUT:second_url"]
+        quality = int(record["OUTPUT:quality"] == "OK")
+        markup[(first_url, second_url)] = quality
+    return markup
+
+
+def get_data_to_cluster(clustering_data_file):
+    url2record = dict()
+    filename2url = dict()
+    with open(clustering_data_file, "r") as r:
+        for line in r:
+            record = json.loads(line)
+            url2record[record["url"]] = record
+            filename2url[record["file_name"]] = record["url"]
+    return url2record, filename2url
+
+
+def calc_clustering_metrics(gold_markup, url2label, url2record, output_dict=False):
+    not_found_count = 0
+    for first_url, second_url in list(gold_markup.keys()):
+        not_found_in_labels = first_url not in url2label or second_url not in url2label
+        not_found_in_records = first_url not in url2record or second_url not in url2record
+        if not_found_in_labels or not_found_in_records:
+            not_found_count += 1
+            gold_markup.pop((first_url, second_url))
+
+    targets = []
+    predictions = []
+    for (first_url, second_url), target in gold_markup.items():
+        prediction = int(url2label[first_url] == url2label[second_url])
+        first = url2record.get(first_url)
+        second = url2record.get(second_url)
+        targets.append(target)
+        predictions.append(prediction)
+
+    return classification_report(targets, predictions, output_dict=output_dict)
